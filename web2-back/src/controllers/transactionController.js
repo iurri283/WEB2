570b10a84faff3bdfde1f9ea4a1e449edcc02579
g4briel-rp <leirbagrp@gmail.com>
2024-09-23 11:27:43 -0300

    falta excluir conta

--- web2-back/src/controllers/transactionController.js
+++ web2-back/src/controllers/transactionController.js
@@ -1,4 +1,7 @@
-const { getTransactionService } = require("../services/transactionService");
+const {
+  getTransactionService,
+  getNomeService,
+} = require("../services/transactionService");
 
 const getTransaction = async (req, res) => {
   const { cpf } = req.userInfo;
@@ -11,4 +14,15 @@ const getTransaction = async (req, res) => {
   }
 };
 
-module.exports = { getTransaction };
+const getNome = async (req, res) => {
+  const { idConta } = req.query;
+
+  try {
+    const nome = await getNomeService(idConta);
+    res.status(200).json({ nome: nome });
+  } catch (error) {
+    res.status(error.status || 500).json({ mensagem: error.message });
+  }
+};
+
+module.exports = { getTransaction, getNome };
--- web2-front/src/pages/Extrato.jsx
+++ web2-front/src/pages/Extrato.jsx
@@ -13,7 +13,7 @@ import useAccount from "../hooks/useAccount";
 import { api } from "../utils/api";
 import { useEffect, useState } from "react";
 import moment from "moment"; // Para formatação de datas
-import useUser from "../hooks/useUser";
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 
 const drawerWidth = 240;
 
@@ -21,74 +21,209 @@ function Extrato() {
   const token = localStorage.getItem("token");
   const account = useAccount();
   const [extrato, setExtrato] = useState([]);
-
-  const user = useUser();
-  console.log(user);
+  const [nomes, setNomes] = useState({});
+  const [filtro, setFiltro] = useState("");
 
   const getExtrato = async () => {
     const resposta = await api.get(`transacoes/getTransactions`);
-    console.log(resposta?.data?.transactions || []);
-    setExtrato(resposta?.data?.transactions || []);
+    const transactions = resposta?.data?.transactions || [];
+    setExtrato(transactions);
+
+    const nomesTemp = {};
+    for (const transaction of transactions) {
+      if (transaction.tipoTransacao === "TRANSFERENCIA") {
+        const idConta =
+          transaction.contaDestino_idConta === account.idConta
+            ? transaction.conta_idConta
+            : transaction.contaDestino_idConta;
+        if (!nomesTemp[idConta]) {
+          const nomeResposta = await api.get(
+            `transacoes/getNome?idConta=${idConta}`
+          );
+          nomesTemp[idConta] = nomeResposta?.data?.nome[0]?.nomeUsuario || "";
+        }
+      }
+    }
+    setNomes(nomesTemp);
   };
+
+  const handleFiltroChange = (event) => {
+    setFiltro(event.target.value);
+  };
+
+  const [dataInicio, setDataInicio] = useState("");
+  const [dataFim, setDataFim] = useState("");
+
+  const handleDataInicioChange = (event) => {
+    setDataInicio(event.target.value);
+  };
+
+  const handleDataFimChange = (event) => {
+    setDataFim(event.target.value);
+  };
+
+  const extratoFiltrado = extrato.filter((transacao) => {
+    if (!filtro && !dataInicio && !dataFim) return true;
+
+    const dataTransacao = moment(transacao.dataTransacao);
+    const isTipoMatch = filtro ? transacao.tipoTransacao === filtro : true;
+    const isDataInicioMatch = dataInicio
+      ? dataTransacao.isSameOrAfter(moment(dataInicio))
+      : true;
+    const isDataFimMatch = dataFim
+      ? dataTransacao.isSameOrBefore(moment(dataFim))
+      : true;
+
+    return isTipoMatch && isDataInicioMatch && isDataFimMatch;
+  });
 
   useEffect(() => {
     if (token) getExtrato();
   }, [token]);
 
   return (
-    <>
-      <Box
+    <Box
+      sx={{
+        display: "flex",
+        bgcolor: "#f5f5f5",
+        height: "100vh",
+        "& .css-i7soq9-MuiPaper-root-MuiAppBar-root": { boxShadow: "none" },
+      }}
+    >
+      <CssBaseline />
+      <AppBar
+        position="fixed"
         sx={{
-          display: "flex",
-          bgcolor: "#f5f5f5",
-          height: "100vh",
-          "& .css-i7soq9-MuiPaper-root-MuiAppBar-root": { boxShadow: "none" },
+          width: `calc(100% - ${drawerWidth}px)`,
+          ml: `${drawerWidth}px`,
+          bgcolor: "#008C9E",
         }}
       >
-        <CssBaseline />
-        <AppBar
-          position="fixed"
-          sx={{
-            width: `calc(100% - ${drawerWidth}px)`,
-            ml: `${drawerWidth}px`,
-            bgcolor: "#008C9E",
-          }}
-        >
-          <Toolbar style={{ display: "flex", justifyContent: "end" }}>
-            <Typography variant="h6" noWrap component="div">
-              {account?.saldoConta?.toLocaleString("pt-BR", {
-                style: "currency",
-                currency: "BRL",
-              }) || "R$ 0,00"}
-            </Typography>
-          </Toolbar>
-        </AppBar>
+        <Toolbar style={{ display: "flex", justifyContent: "end" }}>
+          <Typography variant="h6" noWrap component="div">
+            {account?.saldoConta?.toLocaleString("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            }) || "R$ 0,00"}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          bgcolor: "#f5f5f5",
+          width: `calc(100% - ${drawerWidth}px)`,
+          marginTop: "30px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Toolbar style={{ position: "fixed", top: 0 }} />
 
         <Box
-          component="main"
-          sx={{
-            flexGrow: 1,
-            bgcolor: "#f5f5f5",
-            width: `calc(100% - ${drawerWidth}px)`,
-            marginTop: "30px",
+          sx={{ mt: 4 }}
+          style={{
             display: "flex",
             flexDirection: "column",
+            justifyContent: "center",
             alignItems: "center",
           }}
         >
-          <Toolbar style={{ position: "fixed", top: 0 }} />
+          <h2>Extrato Bancário</h2>
 
           <Box
-            sx={{ mt: 5 }}
+            sx={{ mt: 2 }}
             style={{
               display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
+              flexDirection: "row",
+              justifyContent: "left",
             }}
           >
-            <h2>Extrato Bancário</h2>
+            <FormControl sx={{ minWidth: 200, mb: 2 }}>
+              <InputLabel shrink htmlFor="filtro-label">
+                Filtrar por Tipo
+              </InputLabel>
+              <Select
+                labelId="filtro-label"
+                id="filtro"
+                value={filtro}
+                label="Filtrar por Tipo"
+                onChange={handleFiltroChange}
+              >
+                <MenuItem value="">
+                  <em>Todos</em>
+                </MenuItem>
+                <MenuItem value="DEPOSITO">Depósito</MenuItem>
+                <MenuItem value="SAQUE">Saque</MenuItem>
+                <MenuItem value="TRANSFERENCIA">Transferência</MenuItem>
+              </Select>
+            </FormControl>
 
+            <FormControl sx={{ minWidth: 200, mb: 2, ml: 2 }}>
+              <InputLabel shrink htmlFor="data-inicio">
+                Data Início
+              </InputLabel>
+              <input
+                type="date"
+                id="data-inicio"
+                value={dataInicio}
+                onChange={handleDataInicioChange}
+                style={{
+                  width: "100%",
+                  padding: "10px",
+                  borderRadius: "4px",
+                  border: "1px solid #ccc",
+                }}
+              />
+            </FormControl>
+
+            <FormControl sx={{ minWidth: 200, mb: 2, ml: 2 }}>
+              <InputLabel shrink htmlFor="data-fim">
+                Data Fim
+              </InputLabel>
+              <input
+                type="date"
+                id="data-fim"
+                value={dataFim}
+                onChange={handleDataFimChange}
+                style={{
+                  width: "100%",
+                  padding: "10px",
+                  borderRadius: "4px",
+                  border: "1px solid #ccc",
+                }}
+              />
+            </FormControl>
+
+            <FormControl sx={{ mb: 2, ml: 2 }}>
+              <button
+                onClick={() => {
+                  setFiltro("");
+                  setDataInicio("");
+                  setDataFim("");
+                }}
+                style={{
+                  padding: "10px 20px",
+                  borderRadius: "4px",
+                  border: "none",
+                  backgroundColor: "#008C9E",
+                  color: "white",
+                  cursor: "pointer",
+                }}
+              >
+                Resetar Filtros
+              </button>
+            </FormControl>
+          </Box>
+
+          {extratoFiltrado.length === 0 ? (
+            <Typography variant="body1" sx={{ mt: 2 }}>
+              Não foi realizada nenhuma transação nesta conta.
+            </Typography>
+          ) : (
             <List
               sx={{
                 width: "80vw",
@@ -98,65 +233,82 @@ function Extrato() {
                 boxShadow: "0 2px 5px rgba(0, 0, 0, 0.1)",
               }}
             >
-              {extrato.map((element, index) => (
-                <div key={index}>
-                  <ListItem
-                    sx={{
-                      display: "flex",
-                      justifyContent: "space-between",
-                      padding: "16px",
-                      backgroundColor:
-                        element.tipoTransacao === "DEPOSITO" ||
-                        element.contaDestino_idConta === user.idUsuario
-                          ? "#e6f7e6"
-                          : "#fbeaea",
-                    }}
-                  >
-                    <ListItemText
-                      primary={
-                        <Typography variant="body2" sx={{ fontWeight: "bold" }}>
-                          {element.tipoTransacao === "TRANSFERENCIA" &&
-                          element.contaDestino_idConta === user.idUsuario
-                            ? "TRANSFERENCIA RECEBIDA"
-                            : element.tipoTransacao === "TRANSFERENCIA"
-                            ? "TRANSFERENCIA ENVIADA"
-                            : element.tipoTransacao}
-                        </Typography>
-                      }
-                      secondary={moment(element.dataTransacao).format(
-                        "DD/MM/YYYY HH:mm"
-                      )}
-                    />
-                    <Typography
-                      variant="body1"
+              {extratoFiltrado.map((element, index) => {
+                const nomeUsuario =
+                  element.tipoTransacao === "TRANSFERENCIA"
+                    ? element.contaDestino_idConta === account.idConta
+                      ? nomes[element.conta_idConta]
+                      : nomes[element.contaDestino_idConta]
+                    : "";
+
+                return (
+                  <div key={index}>
+                    <ListItem
                       sx={{
-                        color:
+                        display: "flex",
+                        justifyContent: "space-between",
+                        padding: "16px",
+                        backgroundColor:
                           element.tipoTransacao === "DEPOSITO" ||
-                          element.contaDestino_idConta === user.idUsuario
-                            ? "green"
-                            : "red",
-                        fontWeight: "bold",
+                          (element.tipoTransacao === "TRANSFERENCIA" &&
+                            element.contaDestino_idConta === account.idConta)
+                            ? "#e6f7e6"
+                            : "#fbeaea",
                       }}
                     >
-                      {element.tipoTransacao === "DEPOSITO" ||
-                      element.contaDestino_idConta === user.idUsuario
-                        ? "+"
-                        : "-"}{" "}
-                      R$
-                      {element.valorTransacao.toLocaleString("pt-BR", {
-                        minimumFractionDigits: 2,
-                        maximumFractionDigits: 2,
-                      })}
-                    </Typography>
-                  </ListItem>
-                  <Divider />
-                </div>
-              ))}
+                      <ListItemText
+                        primary={
+                          <Typography
+                            variant="body2"
+                            sx={{ fontWeight: "bold" }}
+                          >
+                            {element.tipoTransacao === "TRANSFERENCIA" &&
+                            element.contaDestino_idConta === account.idConta
+                              ? "TRANSFERENCIA RECEBIDA DE " +
+                                (nomeUsuario ? nomeUsuario.toUpperCase() : "")
+                              : element.tipoTransacao === "TRANSFERENCIA"
+                              ? "TRANSFERENCIA ENVIADA PARA " +
+                                (nomeUsuario ? nomeUsuario.toUpperCase() : "")
+                              : element.tipoTransacao}
+                          </Typography>
+                        }
+                        secondary={moment(element.dataTransacao).format(
+                          "DD/MM/YYYY HH:mm"
+                        )}
+                      />
+                      <Typography
+                        variant="body1"
+                        sx={{
+                          color:
+                            element.tipoTransacao === "DEPOSITO" ||
+                            (element.tipoTransacao === "TRANSFERENCIA" &&
+                              element.contaDestino_idConta === account.idConta)
+                              ? "green"
+                              : "red",
+                          fontWeight: "bold",
+                        }}
+                      >
+                        {element.tipoTransacao === "DEPOSITO" ||
+                        (element.tipoTransacao === "TRANSFERENCIA" &&
+                          element.contaDestino_idConta === account.idConta)
+                          ? "+"
+                          : "-"}{" "}
+                        R$
+                        {element.valorTransacao.toLocaleString("pt-BR", {
+                          minimumFractionDigits: 2,
+                          maximumFractionDigits: 2,
+                        })}
+                      </Typography>
+                    </ListItem>
+                    <Divider />
+                  </div>
+                );
+              })}
             </List>
-          </Box>
+          )}
         </Box>
       </Box>
-    </>
+    </Box>
   );
 }
 
